b6081d2d379edb53bad5af5818fb6caa5e1653fa
shekh khwaja julaha <khwajajulaha122@gmail.com>
2026-01-28 22:42:03 +0530

    myport

---
+++ myportfolio/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Nav from './components/Nav/Nav'
+import About from './components/About/About'
+import Home from './components/Home/Home'
+
+function App() {
+  return (
+    <div>
+      <Nav/>
+      <About/>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
---
+++ myportfolio/src/components/Nav/Nav.jsx
@@ -0,0 +1,33 @@
+import React, { useRef } from 'react'
+import './Nav.css'
+import { Link } from 'react-scroll'
+function Nav() {
+    let menu=useRef()
+    let mobile=useRef()
+  return (
+    <nav>
+        <h1>PORTFOLIO</h1>
+        <ul className='desktopmenu'>
+           <Link to="home"  activeClass='active'spy={true} smooth={true} duration={500}><li>Home</li></Link>
+            <Link to="Contacts"activeClass='active'spy={true} smooth={true} duration={500}><li>Contacts</li></Link>
+           <Link to="about" activeClass='active'spy={true} smooth={true} duration={500}><li>About</li></Link>
+           <Link to="Projects"activeClass='active'spy={true} smooth={true} duration={500}><li>Projects</li></Link>
+        </ul>
+        <div className="hamburger" ref={menu} onClick={()=>{
+          mobile.current.classList.toggle("activemobile")
+        }}>
+            <div className="ham"></div>
+            <div className="ham"></div>
+            <div className="ham"></div>
+        </div>
+          <ul className='mobilemenu' ref={mobile}>
+           <Link to="home"  activeClass='active'spy={true} smooth={true} duration={500}><li>Home</li></Link>
+            <Link to="Contacts"activeClass='active'spy={true} smooth={true} duration={500}><li>Contacts</li></Link>
+           <Link to="about" activeClass='active'spy={true} smooth={true} duration={500}><li>About</li></Link>
+           <Link to="Projects"activeClass='active'spy={true} smooth={true} duration={500}><li>Projects</li></Link>
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
